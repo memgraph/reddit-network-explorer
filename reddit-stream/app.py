b1fb57b2b9fd537a5a1ee173f8a0baf74465809d
Antonio Andelic <antonio.andelic@memgraph.io>
2021-08-25 13:04:48 +0200

    define setup workflow with docker

--- reddit-stream/app.py
+++ reddit-stream/app.py
@@ -1,12 +1,8 @@
 from argparse import ArgumentParser
-from kafka.admin import KafkaAdminClient, NewTopic
 from kafka import KafkaProducer
-from kafka.errors import TopicAlreadyExistsError, NoBrokersAvailable
-from time import sleep
 import praw
 import pickle
 from multiprocessing import Process
-from gqlalchemy import Memgraph
 
 
 KAFKA_ENDPOINT='kafka:9092' 
@@ -61,55 +57,9 @@ def produce_submissions(reddit, subreddit):
         print(submission_info)
         producer.send('submissions', pickle.dumps(submission_info))
 
-
-def get_admin_client():
-    retries = 10
-    while True:
-        try:
-            admin_client = KafkaAdminClient(
-                bootstrap_servers=KAFKA_ENDPOINT,
-                client_id="reddit-stream")
-            return admin_client
-        except NoBrokersAvailable:
-            retries -= 1
-            if not retries:
-                raise
-            print("Failed to connect to Kafka")
-            sleep(1)
-
         
 def main():
     args = parse_args()
-
-    admin_client = get_admin_client()
-    print("Connected to Kafka")
-
-    topic_list = [
-        NewTopic(
-            name="comments",
-            num_partitions=1,
-            replication_factor=1),
-        NewTopic(
-            name="submissions",
-            num_partitions=1,
-            replication_factor=1)]
-    
-    try:
-        admin_client.create_topics(new_topics=topic_list, validate_only=False)
-    except TopicAlreadyExistsError:
-      pass 
-    print("Created topics")
-
-    print("Connecting to Memgraph")
-    memgraph = Memgraph()
-    print("Creating stream connections on Memgraph")
-    memgraph.execute("CREATE STREAM comment_stream TOPICS comments TRANSFORM reddit.comments")
-    memgraph.execute("START STREAM comment_stream")
-    memgraph.execute("CREATE STREAM submission_stream TOPICS submissions TRANSFORM reddit.submissions")
-    memgraph.execute("START STREAM submission_stream")
-
-    print("Creating triggers on Memgraph")
-    memgraph.execute("CREATE TRIGGER created_trigger ON CREATE AFTER COMMIT EXECUTE CALL publisher.create(createdObjects)")
 
     print("Start fetching data from Reddit")
 
