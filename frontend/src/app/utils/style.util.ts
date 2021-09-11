1f3277925a0edd9315d9cf09d7b242bc05e94d0d
David <davidlozic@gmail.com>
2021-09-11 17:23:29 +0200

    Polish code structure, add minor fixes (#13)
    * Rename chat to live-feed, add utility function
    * Move components to src/components, fix minor issues
    * Add try/catch for physics errors, fix live-feed scss
    * Remove d3 transition and use css
    * Add minor styling updates

---
+++ frontend/src/app/utils/style.util.ts
@@ -0,0 +1,29 @@
+// Returns D3 compatible styles for specific node type.
+export function getStyle(type, sentiment) {
+  if (type === 'COMMENT') {
+    const radius = 7;
+    let color = '#989898';
+    if (sentiment === -1) {
+      color = '#ff0000';
+    }
+    if (sentiment === 1) {
+      color = '#00ff00';
+    }
+    return { color, radius };
+  }
+  if (type === 'REDDITOR') {
+    return { color: '#0000ff', radius: 5 };
+  }
+  if (type === 'SUBMISSION') {
+    const radius = 10;
+    let color = '#585858';
+    if (sentiment === -1) {
+      color = '#F2543D';
+    }
+    if (sentiment === 1) {
+      color = '#38C477';
+    }
+    return { color, radius };
+  }
+  return { color: '#ef42f5', radius: 3 };
+}
